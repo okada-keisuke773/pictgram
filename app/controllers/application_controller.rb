b10031facaa9a9e9779f035d67f4b1f59d5710d3
岡田佳佑 <k-okada@cmind-co.jp>
2020-09-16 16:17:08 +0900

    第3章 - Rails実践 - 第8部 - ログイン機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,16 @@
 class ApplicationController < ActionController::Base
-protect_from_forgery with: :exception
+    protect_from_forgery with: :exception
+    add_flash_types :success, :info, :warning, :danger
 
-add_flash_types :success, :info, :warning, :danger
+    # Dry
+    helper_method :current_user, :logged_in?
+
+    def current_user
+        @current_user ||= User.find_by(id: session[:user_id])
+    end
+
+    def logged_in?
+        !current_user.nil?
+    end
 end
 
