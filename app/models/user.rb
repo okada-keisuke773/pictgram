28e92c02a887116fd67610660c9c392fa4ea125f
岡田佳佑 <k-okada@cmind-co.jp>
2020-09-17 12:50:02 +0900

    第3章 - Rails実践 - 第9部 - 写真投稿機能の作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,6 @@ class User < ApplicationRecord
     VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])[a-z\d]{8,32}+\z/
     validates :password, presence: true, length: { minimum: 8, maximum: 32}, format: { with: VALID_PASSWORD_REGEX}
     has_secure_password
+
+    has_many :topics
 end
